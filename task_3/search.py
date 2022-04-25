633afe80ca4363cf5a85bafbc40a8ebdd9f905ff
ksasazawa <k.sasazawa@estyle-inc.jp>
2022-04-25 13:34:27 +0900

    6番までをコミット

--- task_3/search.py
+++ task_3/search.py
@@ -1,24 +1,45 @@
-#テスト
+import os
+import csv
 import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+
+def kimetsu_search(word, file_name):
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
-    source=list(df["name"])
+    # 既に該当ファイルがあった場合
+    if os.path.exists(f"./{file_name}"):
+        df=pd.read_csv(f"./{file_name}")
+        source=list(df["name"])
+    # 該当ファイルがなかった場合
+    else:
+        with open(f"./{file_name}", "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(['no', 'name'])
+        f.close()
+        df=pd.read_csv(f"./{file_name}")
+        source=list(df["name"])
 
     # 検索
+    # CSVに該当ワードがあった場合
     if word in source:
-        print("『{}』はあります".format(word))
+        # CSV書き込み
+        df=pd.DataFrame(source,columns=["name"])
+        df.to_csv(f"./{file_name}",encoding="utf_8-sig")
+        print("『{}』はあります".format(word)) 
+        print(source)     
+        return "『{}』はあります".format(word)
+    # CSVに該当ワードがなかった場合
     else:
+        source.append(word)
+        # CSV書き込み
+        df=pd.DataFrame(source,columns=["name"])
+        df.to_csv(f"./{file_name}",encoding="utf_8-sig")
         print("『{}』はありません".format(word))
+        print(source)
+        return "『{}』はありません".format(word)
         # 追加
         #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
         #if add_flg=="1":
-        source.append(word)
     
-    # CSV書き込み
-    df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
-    print(source)
+
