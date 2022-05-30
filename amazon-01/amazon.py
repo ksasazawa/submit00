35911efbc2f2eab5368cbb11aeb32f01683f2768
ksasazawa <k.sasazawa@estyle-inc.jp>
2022-05-30 22:36:11 +0900

    修正

--- amazon-01/amazon.py
+++ amazon-01/amazon.py
@@ -62,44 +62,53 @@ def main():
     count = 1
     
     for asin in df['ASIN']:
-        driver.get(url.format(asin=asin))
-        image_url = driver.find_element(by=By.ID, value="landingImage").get_attribute("src")
-        name = driver.find_element(by=By.CSS_SELECTOR, value=".product-title-word-break").text
-        try:
-            review_count = driver.find_element(by=By.CSS_SELECTOR, value="#reviewsMedley > div > div.a-fixed-left-grid-col.a-col-left > div.a-section.a-spacing-none.a-spacing-top-mini.cr-widget-ACR > div.a-row.a-spacing-medium.averageStarRatingNumerical > span").text.rstrip("件のグローバル評価")
-            review = driver.find_element(by=By.CSS_SELECTOR, value="    #reviewsMedley > div > div.a-fixed-left-grid-col.a-col-left > div.a-section.a-spacing-none.a-spacing-top-mini.cr-widget-ACR > div.a-fixed-left-grid.AverageCustomerReviews.a-spacing-small > div > div.a-fixed-left-grid-col.aok-align-center.a-col-right > div > span > span").text.lstrip("星5つ中の")      
-        except:
-            review_count = 0
-            review = ""
-        price = ""
-        prime = ""
-        shipper =""
-        
-        try:
-            normal_order = driver.find_element(by=By.ID, value="sfsb_accordion_head")
-        except:
-            normal_order = ""
-        # バーム
-        if normal_order != "":
-            shipper = normal_order.find_elements(by=By.TAG_NAME, value="span")[1].text
-            if shipper in "Amazon":
-                price = driver.find_element(by=By.CSS_SELECTOR, value="#corePrice_feature_div > div > span.a-price.a-text-price.header-price.a-size-base.a-text-normal > span:nth-child(2)").text.lstrip("￥")
-                prime = "prime"
-        else:
-            a_box = driver.find_element(by=By.CSS_SELECTOR, value=".a-box-inner")
+        try:     
+            driver.get(url.format(asin=asin))
+            name = driver.find_element(by=By.CSS_SELECTOR, value=".product-title-word-break").text
+            image_url = driver.find_element(by=By.ID, value="landingImage").get_attribute("src")
+            price = ""
+            prime = ""
             try:
+                review_count = driver.find_element(by=By.CSS_SELECTOR, value="#reviewsMedley > div > div.a-fixed-left-grid-col.a-col-left > div.a-section.a-spacing-none.a-spacing-top-mini.cr-widget-ACR > div.a-row.a-spacing-medium.averageStarRatingNumerical > span").text.rstrip("件のグローバル評価")
+                review = driver.find_element(by=By.CSS_SELECTOR, value="#reviewsMedley > div > div.a-fixed-left-grid-col.a-col-left > div.a-section.a-spacing-none.a-spacing-top-mini.cr-widget-ACR > div.a-fixed-left-grid.AverageCustomerReviews.a-spacing-small > div > div.a-fixed-left-grid-col.aok-align-center.a-col-right > div > span > span").text.lstrip("星5つ中の")      
+            except:
+                review_count = 0
+                review = ""
+            shipper =""
+            try:
+                normal_order = driver.find_element(by=By.ID, value="sfsb_accordion_head")
+            except:
+                normal_order = ""
+            
+            # 公式でない場合    
+            if normal_order == "":
+                a_box = driver.find_element(by=By.CSS_SELECTOR, value=".a-box-inner")
                 rows = a_box.find_elements(by=By.CSS_SELECTOR, value=".tabular-buybox-text")
                 for row in rows:
                     if row.get_attribute("tabular-attribute-name") == "出荷元" and row.text in "Amazon":
                         prime = "prime"
-                        # アーチピロー
+                        # 黒字の値段
                         try:
                             price = a_box.find_element(by=By.CSS_SELECTOR, value=".a-price-whole").text
-                        # ウルみえ
+                        # 赤字の値段
                         except:
                             price = driver.find_element(by=By.CSS_SELECTOR, value="#corePrice_feature_div > div > span.a-price.a-text-price.a-size-medium > span:nth-child(2)").text.lstrip("￥")
-            except:
-                pass
+            # 公式販売ページの場合
+            else:
+                shipper = normal_order.find_elements(by=By.TAG_NAME, value="span")[1].text
+                if shipper in "Amazon":
+                    price = driver.find_element(by=By.CSS_SELECTOR, value="#corePrice_feature_div > div > span.a-price.a-text-price.header-price.a-size-base.a-text-normal > span:nth-child(2)").text.lstrip("￥")
+                    prime = "prime"
+            log(f"{count}件目【成功！】：{name}")
+        
+        except:
+            name = ""
+            price = ""
+            prime = ""
+            review_count = ""
+            review = ""
+            image_url = ""
+            log(f"{count}件目【失敗、、、】：{name}")
                             
         item_info.append({
             "ASIN": asin,
@@ -111,10 +120,6 @@ def main():
             "評価": review,
             "画像URL": image_url
         })
-        if prime == "prime":
-            log(f"{count}件目【prime！】：{name}")
-        else:
-            log(f"{count}件目【no prime】：{name}")
         count+=1
 
     # CSVファイル保存処理
